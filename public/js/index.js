761639d018bf0c196ac38c3fdd1aba5d47fd3886
Gart <gmhobson@gmail.com>
2022-06-26 18:54:29 -0500

    change service worker scope

--- public/js/index.js
+++ public/js/index.js
@@ -5,7 +5,7 @@ fetch("/api/transaction")
   .then(response => {
     return response.json();
   })
-  .then(data => {
+  .then((data) => {
     // save db data on global variable
     transactions = data;
 
